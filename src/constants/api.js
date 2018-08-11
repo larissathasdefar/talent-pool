c1a3bfdc7abb468e8eac3b9a3a26e3c1f5119d2b
Larissa Farias <larissathasdefar@hotmail.com>
2018-08-10 22:43:22 -0300

    Changed README

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,4 +1,3 @@
 const api_test = 'https://api-test-vanhack.azurewebsites.net/api/services/app'
-// const api_prod = 'https://api-vanhack-prod.azurewebsites.net/api/services/app'
 
 export default api_test
